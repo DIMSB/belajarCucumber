71905b8d571be5d6c10794ce0246ffc5d9c5cb7e
DIMSB <dimaspinem768@student.ub.ac.id>
2025-06-12 13:39:29 +0700

    First commit: upload cucumber project

---
+++ src/test/java/com/Dimas/stepdef/MyStepdefs.java
@@ -0,0 +1,28 @@
+package com.Dimas.stepdef;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class MyStepdefs {
+    @Given("user is on login page")
+    public void userIsOnLoginPage() {
+    }
+
+    @When("user input email text box with {string}")
+    public void userInputEmailTextBoxWith(String arg0) {
+    }
+
+    @And("user input password text box with {string}")
+    public void userInputPasswordTextBoxWith(String arg0) {
+    }
+
+    @And("user click login button")
+    public void userClickLoginButton() {
+    }
+
+    @Then("User will redirect to homepage")
+    public void userWillRedirectToHomepage() {
+    }
+}
---
+++ src/test/java/com/Dimas/stepdef/cucumberTest.java
@@ -0,0 +1,17 @@
+package com.Dimas.stepdef;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        glue = {"com.Dimas.stepdef"},
+        features = {"src/test/resources"},
+        plugin = {"pretty"}
+)
+public class cucumberTest {
+
+
+}
